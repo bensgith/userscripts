8e8804a61d21ac13014bc3f53227d15bb7b38161
Benjamin L <benbxliang@gmail.com>
2024-06-25 19:08:17 +0800

    hide more elements

--- src/vocabulary.user.js
+++ src/vocabulary.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vocabulary Ehancement
 // @namespace    https://github.com/bensgith/tampermonkey-scripts/
-// @version      0.0.2
+// @version      0.0.3
 // @description  Remove / hide unecessary infomation
 // @author       Benjamin L
 // @match        https://www.vocabulary.com/dictionary/*
@@ -13,17 +13,29 @@
     'use strict';
 
     let css = `
-        .dictionary_home .box-subscribe,
-        .sign-up-tile-area,
         .page-footer,
+        .dictionary_home .box-subscribe,
+        .dictionary_home .dictionary-quiz,
         .wordPage .vcom_progress,
-        .wordPage .col-2 .vocab-trainer-background {
+        .wordPage .col-2 .vocab-trainer-background,
+        .sign-up-area,
+        .sign-up-tile-area,
+        .loggedout .sign-up-area {
             display: none;
         }
         .promote-subscribe-dictionary {
             display: none !important;
         }
+        .dictionary_home .word-of-the-day {
+            width: unset;
+        }
     `;
 
     GM_addStyle(css);
+
+    window.addEventListener('load', function() {
+        let vocabTrainer = document.querySelector('.wordPage .col-2 .vocab-trainer-background');
+        vocabTrainer.removeAttribute('style');
+    });
+
 })();
