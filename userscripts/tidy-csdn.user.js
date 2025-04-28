b9c500d7391ce64d02e38d912e2f087ef55e2f05
Benjamin Win <benbxliang@gmail.com>
2025-04-28 21:50:35 +0800

    feat: add Tidy CSDN user script

---
+++ userscripts/tidy-csdn.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Tidy CSDN
+// @namespace    https://github.com/bensgith/mytoolbox
+// @version      0.0.1
+// @description  Remove annoying bars and boxes
+// @author       Benjamin L.
+// @match        https://blog.csdn.net/*
+// @icon         https://g.csdnimg.cn/static/logo/favicon32.ico
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`
+        .passport-login-container,
+        .passport-login-tip-container,
+        .more-toolbar,
+        .recommend-box,
+        .csdn-side-toolbar,
+        .blog_container_aside {
+            display: none;
+        }
+
+        main {
+            width: auto !important;
+        }
+    `);
+})();
